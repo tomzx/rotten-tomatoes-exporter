de53e4a95b5976ae30193f26a9830e0bf4d1c3ff
Tom Rochette <roctom@gmail.com>
2013-05-05 13:00:31 -0400

    Initial commit.
    - README.md - How to use the exporter
    - exporter.php - The exporter itself
    - license.txt - The MIT license that covers the code

---
+++ README.md
@@ -0,0 +1,61 @@
+Rotten Tomatoes ratings exporter
+=========
+
+The Rotten Tomatoes exporter is pretty straightforward. It allows you to extract your ratings from the website and save it as a json result.
+
+This exporter was written so that I could export my ratings and import them later on into other movie tracking websites (such as IMDB).
+
+How to use
+----------
+
+In order to use the exporter, you will need to do a couple of things:
+
+1. You need to figure out 2 values that you will need to pass to the constructor of RottenTomatoesExporter. You can get those two values by logging into your Rotten Tomatoes account and then finding the generated cookies key=value required.Those two keys are *session_id* and *fbsr_ID* (where ID is going to be specific to your account).
+
+2. Write a simple script that will load the exporter, set the required values and then finally call the exporter.
+
+```php
+<?php
+require_once 'exporter.php';
+
+// Change SESSION_ID_FROM_COOKIES
+$session_id = 'SESSION_ID_FROM_COOKIES';
+// Change fbsr_ID and FBSR_VALUE_FROM_COOKIES
+$fb = array('fbsr_ID' => 'FBSR_VALUE_FROM_COOKIES');
+
+$exporter = new RottenTomatoeExporter($session_id, $fb);
+$exporter->export();
+$exporter->convert_to_json();
+```
+
+Example
+-------
+
+```php
+<?php
+require_once 'exporter.php';
+
+$session_id = 'owjfmriqnm-948506847';
+
+$fb = array('fbsr_556748447624' => 'Pqz6H8gVG4ZDe9ijIfvyui-kEx_7GfRfgoOLrhRfXi8.eyJhbGdvcml0aG0iOiJITUFDLVNIQTI1NiIsImNvZGUiOiJBUUQ5LTE1ZFhxN1ExM0I3eWx2RkFnM0M3NXU3MjJxaFpGY1FzX3QxWWdTWmxBYV9ZNzBRVWNNVWpoUHNHUG5lMTljY1BYOHJPZi1HN1dPaTNyQkdGd3NwOEVwVzBXWDdTaExrMjdabU9iRlRvQWVtbjJ5RnpXWXVJUXJ4U1duRDdwUVNTdmtCMlVGRlFha1ZSWnRHbnpKamFPdVhNZnkyNjR6dlBUSHhaZ0xDRmpzcng4NGVoYnRzc3llSkgtV05TUFBoVGUyYlFfM3BVNmXZXjhjTFk4eFZZa2wyd2J2dmdPdDZRY190aFM1MURTT3RKc1hQaWtHWDN1aC1Idmk8PXhoN1lYS1ZncTlCckV6WURJaWxMeTBNZzR1NWYzM2E0LXk3VnhVbVJ3V2haRXhiR1c0VW5ldjJkRDRrM3V5ZUVxWSIsImlzc3VlZF9hdCI6MTM2NzcxODY5MiwidXNlcl9pZCI6IjUzMzM5MDYzNiJ9');
+
+$exporter = new RottenTomatoeExporter($session_id, $fb);
+$exporter->export();
+$exporter->convert_to_json();
+```
+
+3. Done! You should have a json file generated for which the filename is your user id on Rotten Tomatoes.
+
+JSON output format
+------------------
+
+The JSON output format is pretty simple and minimalist. It is an array of objects, each containing the title of the movie and the rating you gave it.
+
+```javascript
+[
+  {
+    "title": "The Dark Knight",
+    "rating": "5.0"
+  }
+]
+```
---
+++ exporter.php
@@ -0,0 +1,100 @@
+<?php
+
+function http_build_cookie(array $data)
+{
+	$cookie_string = '';
+	foreach ($data as $key => $value)
+	{
+		$cookie_string .= $key.'='.$value.';';
+	}
+	return $cookie_string;
+}
+
+class RottenTomatoesExporter
+{
+	private $user_id = null;
+	private $session_id = null;
+	private $fb = array();
+
+	public function __construct($session_id, array $fb)
+	{
+		$this->session_id = $session_id;
+		list($dnc, $this->user_id) = preg_split('/-/', $session_id);
+		list($dnc, $id) = preg_split('/_/', array_keys($fb)[0]);
+		$fb['fbm_'.$id] = 'base_domain=.www.rottentomatoes.com';
+		$this->fb = $fb;
+	}
+
+	public function export()
+	{
+		echo 'Exporting user_id = '.$this->user_id.', session_id = '.$this->session_id.PHP_EOL;
+
+		$cookie_details = array('session_id' => $this->session_id);
+		$cookie_details = array_merge($cookie_details, $this->fb);
+
+		$context_options = ['http' =>	[
+										'method' => 'GET',
+										'header' => 'Cookie: '.http_build_cookie($cookie_details)
+										]
+							];
+		$context = stream_context_create($context_options);
+
+		$page = 1;
+		$content = '';
+
+		while (true)
+		{
+			echo 'Getting page '.$page.' ...'.PHP_EOL;
+
+			$page_url = 'http://www.rottentomatoes.com/user/id/'.$this->user_id.'/ratings/?ajax=true&profileUserId='.$this->user_id.'&pageNum='.$page.'&sortby=ratingDate';
+			$page_content = file_get_contents($page_url, false, $context);
+
+			if ($page_content === false || $page_content === $content) return;
+
+			// Get content of interest
+			file_put_contents($this->user_id.'_'.$page.'.html', $page_content);
+
+			$content = $page_content;
+			++$page;
+		}
+	}
+
+	public function convert_to_json()
+	{
+		$data = [];
+
+		foreach (glob('*.html') as $file)
+		{
+			echo 'Parsing file '.$file.PHP_EOL;
+			$content = file_get_contents($file);
+
+			if ($content === '') continue;
+
+			// Force ISO-8859-1 encoding
+			$content = '<?xml version="1.0" encoding="iso-8859-1" ?>'.$content;
+
+			$content = str_replace('&', '&amp;', $content);
+
+			$xml = new SimpleXMLElement($content);
+			$result = $xml->xpath('//div[@data-title]');
+
+			foreach ($result as $node)
+			{
+				echo 'Title: '.$node['data-title'].PHP_EOL;
+				echo 'Rating: '.$node['data-score'].PHP_EOL.PHP_EOL;
+
+				$data[] = ['title' => (string)$node['data-title'], 'rating' => (string)$node['data-score']];
+			}
+		}
+
+		usort($data, array(__CLASS__, 'sort_by_title'));
+
+		file_put_contents($this->user_id.'.json', json_encode($data));
+	}
+
+	public static function sort_by_title($a, $b)
+	{
+		if ($a['title'] === $b['title']) return 0;
+		return $a['title'] < $b['title'] ? -1 : 1;
+	}
+}
